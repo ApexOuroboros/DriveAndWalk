6bbe645269e457f94c80706499080924b2de0ad4
ApexOuroboros <114581783+ApexOuroboros@users.noreply.github.com>
2023-03-13 15:09:30 +0000

    space

--- Assets/scripts/vehMove.cs
+++ Assets/scripts/vehMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class vehMove : MonoBehaviour
 {
+    public float fSpeed = 25f, stSpeed = 7.5f, hSpeed = 5f;
+    private float actFSpeed, actStSpeed, actHSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,12 @@ public class vehMove : MonoBehaviour
     void Update()
     {
         
+        actFSpeed = Input.GetAxisRaw("Vertical") * fSpeed;
+        actStSpeed = Input.GetAxisRaw("Horizontal") * stSpeed;
+        actHSpeed = Input.GetAxisRaw("Hover") * hSpeed;
+
+        transform.position += transform.forward * actFSpeed * Time.deltaTime;
+        transform.position += (transform.right * actStSpeed * Time.deltaTime) + (transform.up * actHSpeed * Time.deltaTime);
+
     }
 }
